b2060c4b07b2776ed78e49ce2ff44b08172757a1
Vincent Parrett <vincent@finalbuilder.com>
2026-03-30 12:53:37 +1100

    We should be monitoring dpr files
    doh

--- src/VSoft.ExternalModDetector.Consts.pas
+++ src/VSoft.ExternalModDetector.Consts.pas
@@ -4,7 +4,7 @@ interface
 
 const
   cDebounceIntervalMs = 200;
-  cMonitoredExtensions : array[0..6] of string = ('.pas', '.inc', '.dproj', '.dpk', '.h', '.cpp', '.rc');
+  cMonitoredExtensions : array[0..7] of string = ('.pas', '.inc', '.dpr', '.dproj', '.dpk', '.h', '.cpp', '.rc');
   cProjectExtensions : array[0..2] of string = ('.dproj', '.cbproj', '.dpk');
 
 implementation
